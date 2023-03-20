afd6080cf16bf4a7ff5c0a9d2b40b17fbeff5543
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-03-20 22:33:46 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
